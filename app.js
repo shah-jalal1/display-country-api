1f9705ec23c095bc1f1b6425e82956d72817f491
shahjalalcse <shah-jalal1>
2021-02-06 23:18:25 +0600

    some code add

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 fetch('https://restcountries.eu/rest/v2/all')
-.then(res => res.json())
-.then(data => displaycCountries(data));
+    .then(res => res.json())
+    .then(data => displaycCountries(data));
 
-const displaycCountries = countries =>{
+const displaycCountries = countries => {
     const contriesDiv = document.getElementById('countries');
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
+
+    countries.forEach(country => {
         const countryDiv = document.createElement('div');
         countryDiv.className = 'country';
-
-        // const h3 = document.createElement('h3');
-        // h3.innerText = country.name;
-
-        // const p = document.createElement('p');
-        // p.innerText = country.capital;
-        
-        // countryDiv.appendChild(h3);
-        // countryDiv.appendChild(p);
 
         const countryInfo = `
             <h3 class="country-name">${country.name}</h3>
@@ -26,8 +17,35 @@ const displaycCountries = countries =>{
         countryDiv.innerHTML = countryInfo;
         contriesDiv.appendChild(countryDiv);
 
-        // li.innerHTML = country.name;
-        // ul.appendChild(li);
-        
-    }
+    });
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement('div');
+    //     countryDiv.className = 'country';
+
+    //     // const h3 = document.createElement('h3');
+    //     // h3.innerText = country.name;
+
+    //     // const p = document.createElement('p');
+    //     // p.innerText = country.capital;
+
+    //     // countryDiv.appendChild(h3);
+    //     // countryDiv.appendChild(p);
+
+    //     const countryInfo = `
+    //         <h3 class="country-name">${country.name}</h3>
+    //         <p>${country.capital}</p>
+    //     `;
+
+    //     countryDiv.innerHTML = countryInfo;
+    //     contriesDiv.appendChild(countryDiv);
+
+    //     // li.innerHTML = country.name;
+    //     // ul.appendChild(li);
+
+    // }
+
+
+
 }
