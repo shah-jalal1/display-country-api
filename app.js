8cad8fa68898460dd76b3005d0f63a203cf3ac51
shahjalalcse <shah-jalal1>
2021-02-06 23:45:54 +0600

    complete

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ const displaycCountries = countries => {
         const countryInfo = `
             <h3 class="country-name">${country.name}</h3>
             <p>${country.capital}</p>
+            <button onClick="displayCountryDetails('${country.name}')">Details</button>
         `;
 
         countryDiv.innerHTML = countryInfo;
         contriesDiv.appendChild(countryDiv);
 
     });
+
+
 
     // for (let i = 0; i < countries.length; i++) {
     //     const country = countries[i];
@@ -48,4 +51,22 @@ const displaycCountries = countries => {
 
 
 
+}
+
+const displayCountryDetails = names=> {
+    const url = `https://restcountries.eu/rest/v2/name/${names}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryinfo(data[0]));
+}
+
+const renderCountryinfo = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}">
+    `
 }
